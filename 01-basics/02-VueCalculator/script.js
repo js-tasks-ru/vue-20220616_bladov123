0c3c2f1c176be645831c5b2db2c1f206071fe49d
lordb <bladov123@gmail.com>
2022-06-21 12:26:55 +0300

    feat: add task 01-counterBtn + 02-VueCalculate

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,39 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
+import { defineComponent } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+let RootComponent = defineComponent({
+  data() {
+    return {
+      valueOne: '',
+      valueTwo: '',
+      picked: '',
+    };
+  },
+
+  computed: {
+    result() {
+      switch (this.picked) {
+        case 'sum':
+          return this.valueOne + this.valueTwo;
+        case 'subtract':
+          return this.valueOne - this.valueTwo;
+        case 'multiply':
+          return this.valueOne * this.valueTwo;
+        case 'divide':
+          return this.valueOne / this.valueTwo;
+      }
+
+      return '';
+    },
+  },
+
+  methods: {
+    increaseCount() {
+      this.count++;
+    },
+  },
+});
+
+let app = createApp(RootComponent);
+
+let vm = app.mount('#app');
