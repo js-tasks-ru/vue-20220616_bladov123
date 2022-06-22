26907bfa92a500167439ed20b96a8a1aecf370c5
lordb <bladov123@gmail.com>
2022-06-22 08:17:43 +0300

    feat: add fetch request

--- 01-basics/03-MarkedList/script.js
+++ 01-basics/03-MarkedList/script.js
@@ -1,58 +1,61 @@
 import { createApp } from './vendor/vue.esm-browser.js';
 import { defineComponent } from './vendor/vue.esm-browser.js';
 // From https://jsonplaceholder.typicode.com/comments
-const emails = [
-  'Eliseo@gardner.biz',
-  'Jayne_Kuhic@sydney.com',
-  'Nikita@garfield.biz',
-  'Lew@alysha.tv',
-  'Hayden@althea.biz',
-  'Presley.Mueller@myrl.com',
-  'Dallas@ole.me',
-  'Mallory_Kunze@marie.org',
-  'Meghan_Littel@rene.us',
-  'Carmen_Keeling@caroline.name',
-  'Veronica_Goodwin@timmothy.net',
-  'Oswald.Vandervort@leanne.org',
-  'Kariane@jadyn.tv',
-  'Nathan@solon.io',
-  'Maynard.Hodkiewicz@roberta.com',
-  'Christine@ayana.info',
-  'Preston_Hudson@blaise.tv',
-  'Vincenza_Klocko@albertha.name',
-  'Madelynn.Gorczany@darion.biz',
-  'Mariana_Orn@preston.org',
-  'Noemie@marques.me',
-  'Khalil@emile.co.uk',
-  'Sophia@arianna.co.uk',
-  'Jeffery@juwan.us',
-  'Isaias_Kuhic@jarrett.net',
-];
+// const emails = [
+//   'Eliseo@gardner.biz',
+//   'Jayne_Kuhic@sydney.com',
+//   'Nikita@garfield.biz',
+//   'Lew@alysha.tv',
+//   'Hayden@althea.biz',
+//   'Presley.Mueller@myrl.com',
+//   'Dallas@ole.me',
+//   'Mallory_Kunze@marie.org',
+//   'Meghan_Littel@rene.us',
+//   'Carmen_Keeling@caroline.name',
+//   'Veronica_Goodwin@timmothy.net',
+//   'Oswald.Vandervort@leanne.org',
+//   'Kariane@jadyn.tv',
+//   'Nathan@solon.io',
+//   'Maynard.Hodkiewicz@roberta.com',
+//   'Christine@ayana.info',
+//   'Preston_Hudson@blaise.tv',
+//   'Vincenza_Klocko@albertha.name',
+//   'Madelynn.Gorczany@darion.biz',
+//   'Mariana_Orn@preston.org',
+//   'Noemie@marques.me',
+//   'Khalil@emile.co.uk',
+//   'Sophia@arianna.co.uk',
+//   'Jeffery@juwan.us',
+//   'Isaias_Kuhic@jarrett.net',
+// ];
 
 let RootComponent = defineComponent({
   data() {
     return {
-      emails,
+      emails: null,
       search: null,
     };
   },
 
   computed: {
     filtredEmails() {
-      return this.emails.map((email) => {
-        if (this.search === '') {
+      if (this.emails) {
+        return this.emails.map((email) => {
+          if (this.search === '') {
+            return {
+              emailAdress: email,
+            };
+          }
+
           return {
             emailAdress: email,
+            classObj: {
+              marked: email.includes(this.search),
+            },
           };
-        }
-
-        return {
-          emailAdress: email,
-          classObj: {
-            marked: email.includes(this.search),
-          },
-        };
-      });
+        });
+      }
+      return 'undefined';
     },
   },
 });
@@ -60,3 +63,17 @@ let RootComponent = defineComponent({
 let app = createApp(RootComponent);
 
 let vm = app.mount('#app');
+
+async function getEmails() {
+  let response = await fetch('https://jsonplaceholder.typicode.com/comments');
+
+  let json = await response.json();
+
+  json = json.map((e) => e.email);
+
+  json.splice(25);
+
+  vm.emails = json;
+}
+
+getEmails();
