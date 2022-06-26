1468dee2bcdaa1d5b10d4d0d814ddd410d324e0c
lordb <bladov123@gmail.com>
2022-06-26 13:18:43 +0300

    feat: add 02-04 task

--- 02-components/04-MeetupInfo/MeetupInfo.js
+++ 02-components/04-MeetupInfo/MeetupInfo.js
@@ -3,19 +3,73 @@ import { defineComponent } from './vendor/vue.esm-browser.js';
 export default defineComponent({
   name: 'MeetupInfo',
 
+  props: {
+    organizer: {
+      type: String,
+      required: true,
+    },
+
+    place: {
+      type: String,
+      required: true,
+    },
+
+    date: {
+      type: Number,
+      required: true,
+    },
+  },
+
+  computed: {
+    dataEvent() {
+      let ruDate = new Date(this.date).toLocaleDateString();
+
+      if (ruDate.startsWith('0')) {
+        ruDate = ruDate.slice(1);
+      }
+
+      ruDate = ruDate.split('.');
+
+      const mounth = {
+        12: 'December',
+        '01': 'January',
+        '02': 'February',
+        '03': 'March',
+        '04': 'April',
+        '05': 'May',
+        '06': 'June',
+        '07': 'July',
+        '08': 'August',
+        '09': 'September',
+        10: 'October',
+        11: 'November',
+      };
+      ruDate.splice(1, 1, mounth[ruDate[1]]);
+
+      [ruDate[0], ruDate[1]] = [ruDate[1], ruDate[0]];
+
+      ruDate[1] += ',';
+      return ruDate.join(' ') + ' г.';
+    },
+
+    dateTimeNumer() {
+      return new Date(this.date).toLocaleDateString().split('.').reverse().join('-');
+    },
+  },
+
   template: `
     <ul class="meetup-info">
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-user.svg" />
-        Организатор
+        {{ organizer  }}
       </li>
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-map.svg" />
-        Место
+        {{ place }}
       </li>
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-cal-lg.svg" />
-        <time datetime="2020-01-01">1 янв. 2020</time>
+        <time :datetime="dateTimeNumer">{{ dataEvent }}</time>
       </li>
     </ul>`,
 });
