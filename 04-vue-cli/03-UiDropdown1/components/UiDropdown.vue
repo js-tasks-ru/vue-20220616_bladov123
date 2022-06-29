f920b8f92de3b32e25cb7d8302c76f6cf7834b61
lordb <bladov123@gmail.com>
2022-06-29 12:03:45 +0300

    feat: add 04-03 task with 4 loae test

--- 04-vue-cli/03-UiDropdown1/components/UiDropdown.vue
+++ 04-vue-cli/03-UiDropdown1/components/UiDropdown.vue
@@ -1,18 +1,23 @@
 <template>
-  <div class="dropdown dropdown_opened">
-    <button type="button" class="dropdown__toggle dropdown__toggle_icon">
-      <ui-icon icon="tv" class="dropdown__icon" />
-      <span>Title</span>
+  <div class="dropdown" :class="{ dropdown_opened: opened }">
+    <button type="button" class="dropdown__toggle" :class="{ dropdown__toggle_icon: withIcon }" @click="toggleOpen">
+      <ui-icon :icon="currentIcon" class="dropdown__icon" />
+      <span>{{ currentTite || title }}</span>
     </button>
 
-    <div class="dropdown__menu" role="listbox">
-      <button class="dropdown__item dropdown__item_icon" role="option" type="button">
-        <ui-icon icon="tv" class="dropdown__icon" />
-        Option 1
-      </button>
-      <button class="dropdown__item dropdown__item_icon" role="option" type="button">
-        <ui-icon icon="tv" class="dropdown__icon" />
-        Option 2
+    <div v-show="opened" class="dropdown__menu" role="listbox">
+      <button
+        class="dropdown__item"
+        :class="{ dropdown__item_icon: withIcon }"
+        role="option"
+        type="button"
+        v-for="(item, index) in options"
+        :key="index"
+        @click="$emit('update:modelValue', item.value), changeTitle(item.text), chaneIcon(item.icon)"
+        @click.passive="toggleOpen"
+      >
+        <ui-icon :icon="item.icon" class="dropdown__icon" />
+        {{ item.text }}
       </button>
     </div>
   </div>
@@ -20,13 +25,140 @@
 
 <script>
 import UiIcon from './UiIcon';
+export default {
+  name: 'UiDropdown',
+  components: { UiIcon },
 
+  props: {
+    options: {
+      type: Array,
+      required: true,
+    },
+
+    modelValue: {
+      type: String,
+    },
+
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+
+  emits: ['update:modelValue'],
+
+  data() {
+    return {
+      opened: false,
+      currentTite: null,
+      currentIcon: null,
+    };
+  },
+
+  computed: {
+    withIcon() {
+      let result = this.options.find((item) => item.icon);
+
+      return result ? true : false;
+    },
+  },
+
+  methods: {
+    toggleOpen() {
+      this.opened = !this.opened;
+    },
+
+    changeTitle(title) {
+      this.currentTite = title;
+    },
+
+    chaneIcon(icon) {
+      this.currentIcon = icon;
+    },
+  },
+};
+</script>
+
+<!-- =============================== -->
+<!-- <template>
+  <div class="dropdown" :class="{ dropdown_opened: opened }">
+    <button type="button" class="dropdown__toggle" :class="{ dropdown__toggle_icon: withIcon }" @click="toggleOpen">
+      <ui-icon :icon="currentIcon" class="dropdown__icon" />
+      <span>{{ currentTitle }}</span>
+    </button>
+
+    <div v-show="opened" class="dropdown__menu" role="listbox">
+      <button
+        v-for="(item, index) in options"
+        :key="index"
+        class="dropdown__item"
+        :class="{ dropdown__item_icon: withIcon }"
+        role="option"
+        type="button"
+        @click="$emit('update:modelValue', item.value), changeIcon(item.icon)"
+        @click.passive="toggleOpen"
+      >
+        <ui-icon :icon="item.icon" class="dropdown__icon" />
+        {{ item.text }}
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+import UiIcon from './UiIcon';
 export default {
   name: 'UiDropdown',
 
   components: { UiIcon },
+
+  props: {
+    options: {
+      type: Array,
+      required: true,
+    },
+
+    modelValue: {
+      type: String,
+    },
+
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+
+  emits: ['update:modelValue'],
+
+  data() {
+    return {
+      opened: false,
+      currentIcon: null,
+    };
+  },
+
+  computed: {
+    withIcon() {
+      let result = this.options.find((item) => item.icon);
+      return result ? true : false;
+    },
+
+    currentTitle() {
+      return this.modelValue || this.title;
+    },
+  },
+
+  methods: {
+    toggleOpen() {
+      this.opened = !this.opened;
+    },
+
+    changeIcon(icon) {
+      this.currentIcon = icon;
+    },
+  },
 };
-</script>
+</script> -->
 
 <style scoped>
 .dropdown {
