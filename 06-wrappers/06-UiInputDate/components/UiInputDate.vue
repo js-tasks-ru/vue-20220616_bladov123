b22fd96868c01980475112c5ab93f6c0ba0e882a
lordb <bladov123@gmail.com>
2022-07-13 16:17:46 +0300

    feat: add 07-02 task

--- 06-wrappers/06-UiInputDate/components/UiInputDate.vue
+++ 06-wrappers/06-UiInputDate/components/UiInputDate.vue
@@ -1,5 +1,5 @@
 <template>
-  <ui-input v-bind="$attrs" v-model="proxyValue" :type="type">
+  <ui-input v-bind="$attrs" :type="type" :model-value="proxyValue" @input="handleInput">
     <template v-for="slotName in Object.keys($slots)" #[slotName]>
       <slot :name="slotName"></slot>
     </template>
@@ -8,7 +8,6 @@
 
 <script>
 import UiInput from './UiInput';
-import { DateTime } from 'luxon';
 
 export default {
   name: 'UiInputDate',
@@ -32,43 +31,34 @@ export default {
   emits: ['update:modelValue'],
 
   computed: {
-    proxyValue: {
-      get() {
-        if (!this.modelValue) return '';
+    proxyValue() {
+      if (!this.modelValue) return '';
 
-        if (this.type === 'date') {
-          const utcDate = new Date(this.modelValue);
-          const isoDate = utcDate.toISOString().split('T')[0];
-          return isoDate;
-        }
+      if (this.type === 'date') {
+        const utcDate = new Date(this.modelValue);
+        const isoDate = utcDate.toISOString().split('T')[0];
+        return isoDate;
+      }
 
-        if (this.type === 'time') {
-          const utcDate = new Date(this.modelValue);
-          const timeDate = utcDate.toISOString().split('T')[1].slice(0, 5);
-          return timeDate;
-        }
+      if (this.type === 'time') {
+        const utcDate = new Date(this.modelValue);
+        const timeDate = utcDate.toISOString().split('T')[1].slice(0, 5);
+        return timeDate;
+      }
 
-        if (this.type === 'datetime-local') {
-          const utcDate = new Date(this.modelValue);
-          const localDate = utcDate.toISOString().slice(0, 16);
-          return localDate;
-        }
+      if (this.type === 'datetime-local') {
+        const utcDate = new Date(this.modelValue);
+        const localDate = utcDate.toISOString().slice(0, 16);
+        return localDate;
+      }
 
-        return '';
-      },
+      return '';
+    },
+  },
 
-      set(value) {
-        let milisec = null;
-
-        if (this.type === 'time') {
-          let [hours, minute] = value.split(':');
-          milisec = (+hours * 60 + +minute) * 60 * 1000;
-        } else {
-          milisec = DateTime.fromISO(value).toMillis();
-        }
-
-        this.$emit('update:modelValue', milisec);
-      },
+  methods: {
+    handleInput($event) {
+      this.$emit('update:modelValue', $event.target.value === '' ? null : $event.target.valueAsNumber);
     },
   },
 };
--- 07-forms/02-MeetupForm/components/MeetupForm.vue
+++ 07-forms/02-MeetupForm/components/MeetupForm.vue
@@ -1,18 +1,18 @@
 <template>
-  <form class="meetup-form">
+  <form class="meetup-form" @submit.prevent="updateForm">
     <div class="meetup-form__content">
       <fieldset class="meetup-form__section">
         <ui-form-group label="Название">
-          <ui-input name="title" />
+          <ui-input v-model="localMetup.title" name="title" />
         </ui-form-group>
         <ui-form-group label="Дата">
-          <ui-input-date type="date" name="date" />
+          <ui-input-date v-model="localMetup.date" type="date" name="date" />
         </ui-form-group>
         <ui-form-group label="Место">
-          <ui-input name="place" />
+          <ui-input v-model="localMetup.place" name="place" />
         </ui-form-group>
         <ui-form-group label="Описание">
-          <ui-input multiline rows="3" name="description" />
+          <ui-input v-model="localMetup.description" multiline rows="3" name="description" />
         </ui-form-group>
         <ui-form-group label="Изображение">
           <!--
@@ -21,24 +21,25 @@
           -->
           <ui-image-uploader
             name="image"
-            :preview="meetup.image"
-            @select="meetup.imageToUpload = $event"
-            @remove="meetup.imageToUpload = null"
+            :preview="localMetup.image"
+            @select="localMetup.imageToUpload = $event"
+            @remove="localMetup.imageToUpload = null"
           />
         </ui-form-group>
       </fieldset>
 
       <h3 class="meetup-form__agenda-title">Программа</h3>
-      <!--
+
       <meetup-agenda-item-form
-         :key="agendaItem.id"
-         :agenda-item="..."
-         class="meetup-form__agenda-item"
-       />
-       -->
+        v-for="(agendaItem, index) in localMetup.agenda"
+        :key="agendaItem.id"
+        v-model:agenda-item="localMetup.agenda[index]"
+        class="meetup-form__agenda-item"
+        @remove="deleteAgenda(index)"
+      />
 
       <div class="meetup-form__append">
-        <button class="meetup-form__append-button" type="button" data-test="addAgendaItem">
+        <button type="button" class="meetup-form__append-button" data-test="addAgendaItem" @click="addAgenda">
           + Добавить этап программы
         </button>
       </div>
@@ -47,9 +48,16 @@
     <div class="meetup-form__aside">
       <div class="meetup-form__aside-stick">
         <!-- data-test атрибуты используются для поиска нужного элемента в тестах, не удаляйте их -->
-        <ui-button variant="secondary" block class="meetup-form__aside-button" data-test="cancel">Отмена</ui-button>
+        <ui-button
+          variant="secondary"
+          block
+          class="meetup-form__aside-button"
+          data-test="cancel"
+          @click="$emit('cancel')"
+          >Отмена</ui-button
+        >
         <ui-button variant="primary" block class="meetup-form__aside-button" data-test="submit" type="submit">
-          SUBMIT
+          {{ submitText }}
         </ui-button>
       </div>
     </div>
@@ -57,7 +65,7 @@
 </template>
 
 <script>
-// import { cloneDeep } from 'lodash-es';
+import { cloneDeep } from 'lodash-es';
 
 import MeetupAgendaItemForm from './MeetupAgendaItemForm';
 import UiButton from './UiButton';
@@ -65,7 +73,7 @@ import UiFormGroup from './UiFormGroup';
 import UiImageUploader from './UiImageUploader';
 import UiInput from './UiInput';
 import UiInputDate from './UiInputDate';
-// import { createAgendaItem } from '../meetupService';
+import { createAgendaItem } from '../meetupService';
 
 export default {
   name: 'MeetupForm',
@@ -88,6 +96,38 @@ export default {
     submitText: {
       type: String,
       default: '',
+    },
+  },
+
+  emits: ['cancel', 'submit'],
+
+  data() {
+    return {
+      localMetup: cloneDeep(this.meetup),
+    };
+  },
+
+  methods: {
+    addAgenda() {
+      const lastIndex = this.localMetup.agenda.length - 1;
+      const newAgenda = createAgendaItem();
+
+      if (lastIndex > -1) {
+        let prevAgenda = this.localMetup.agenda[lastIndex];
+        newAgenda.startsAt = prevAgenda.endsAt;
+        this.localMetup.agenda.push(newAgenda);
+        return;
+      }
+
+      this.localMetup.agenda.push(newAgenda);
+    },
+
+    deleteAgenda(index) {
+      this.localMetup.agenda.splice(index, 1);
+    },
+
+    updateForm() {
+      this.$emit('submit', cloneDeep(this.localMetup));
     },
   },
 };
